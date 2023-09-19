4071c3145e25ca5d8e786aa0687b053ce68fda3f
diwaHa <diwahar008@gmail.com>
2023-09-19 17:51:34 +0530

    first_commit

---
+++ src/main/java/com/pixeltrice/csvfileapp/CsvFileApplication.java
@@ -0,0 +1,13 @@
+package com.pixeltrice.csvfileapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvFileApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/pixeltrice/csvfileapp/repository/DeveloperTutorialRepository.java
@@ -0,0 +1,9 @@
+package com.pixeltrice.csvfileapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pixeltrice.csvfileapp.model.DeveloperTutorial;
+
+public interface DeveloperTutorialRepository extends JpaRepository<DeveloperTutorial, Integer>{
+
+}
